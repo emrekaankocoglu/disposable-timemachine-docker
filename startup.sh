db222ec49ff11111fdceba2e78791fdb6fe0fe5c
Emre Kaan Koçoğlu <54414209+emrekaankocoglu@users.noreply.github.com>
2022-03-17 13:24:27 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
 apt -y update
 apt install -y samba
+echo "[global]
+	min protocol = SMB2
+	vfs objects = fruit streams_xattr  
+	fruit:metadata = stream
+	fruit:model = MacSamba
+	fruit:posix_rename = yes 
+	fruit:veto_appledouble = no
+	fruit:wipe_intentionally_left_blank_rfork = yes 
+	fruit:delete_empty_adfiles = yes 
+	workgroup = WORKGROUP
+	server string = %h server (Samba, Ubuntu)
+	log file = /var/log/samba/log.%m
+	max log size = 1000
+	logging = file
+	panic action = /usr/share/samba/panic-action %d
+	server role = standalone server
+	obey pam restrictions = yes
+	unix password sync = yes
+	passwd program = /usr/bin/passwd %u
+	passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+	pam password change = yes
+	map to guest = bad user
+
+[timemachine]
+	comment = Timemachine
+	path = /srv/samba/timemachine
+	read only = no
+	browseable = yes
+	writeable = yes
+	guest ok = yes
+	guest user = smbuser
+	force user = smbuser
+	create mode = 0777
+  directory mode = 0777
+	vfs objects = fruit streams_xattr
+	fruit:time machine = yes">/etc/samba/smb.conf
 useradd smbuser -s /usr/sbin/nologin
 (echo "a";echo "a") | smbpasswd -a -s smbuser
 mkdir /srv/samba
 mkdir /srv/samba/timemachine
-chown -R samba /srv/samba
+chown -R smbuser /srv/samba
 chmod -R 777 /srv/samba
 service smbd start
 service nmbd start
